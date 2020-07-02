a1165278ad23111e63b7059f3bb5177b698db385
neonf <laurin.pischke@gmx.de>
2020-07-02 12:37:00 +0200

    first commit

---
+++ Assets/_Scripts/MiniCow.cs
@@ -0,0 +1,63 @@
+﻿/* The Cow class has methods and properties that are similar to those of the
+ * Elephant class but are implemented in a different way
+ */
+
+using UnityEngine;
+
+public class MiniCow : Animal
+{
+    private Color skinColor = Color.black;
+    public override Color SkinColor
+    {
+        get { return skinColor; }
+    }
+
+    private Color noseColor = new Color(0.64f, 0.16f, 0.16f);
+    public override Color NoseColor
+    {
+        get { return noseColor; }
+    }
+
+    private float noseLength = 1.2f;
+    public override float NoseLength
+    {
+        get { return noseLength; }
+    }
+
+    private float scale =1.5f;
+    public override float Scale
+    {
+        get
+        {
+            return scale;
+        }
+        set
+        {
+            if (value < 0.1f)
+            {
+                value = 0.1f;
+            }
+            else if (value > 1.5f)
+            {
+                value = 1.5f;
+            }
+            scale = value;
+        }
+    }
+
+    public override void Sleep()
+    {
+        Debug.Log("HeavyBreathing!");
+    }
+
+    public override string Talk()
+    {
+        return "Moo!";
+    }
+
+    public override GameObject GetEars()
+    {
+        GameObject ears = Resources.Load("CatEars") as GameObject;
+        return ears;
+    }
+}
---
+++ Assets/_Scripts/Sheep.cs
@@ -0,0 +1,63 @@
+﻿/* The Sheep class has methods and properties that are similar to those of the
+ * Elephant class but are implemented in a different way
+ */
+
+using UnityEngine;
+
+public class Sheep : Animal
+{
+    private Color skinColor = Color.white;
+    public override Color SkinColor
+    {
+        get { return skinColor; }
+    }
+
+    private Color noseColor = Color.black;
+    public override Color NoseColor
+    {
+        get { return noseColor; }
+    }
+
+    private float noseLength = 1.1f;
+    public override float NoseLength
+    {
+        get { return noseLength; }
+    }
+
+    private float scale = 1.3f;
+    public override float Scale
+    {
+        get
+        {
+            return scale;
+        }
+        set
+        {
+            if (value < 0.1f)
+            {
+                value = 0.1f;
+            }
+            else if (value > 1.5f)
+            {
+                value = 1.5f;
+            }
+            scale = value;
+        }
+    }
+
+    public override void Sleep()
+    {
+        Debug.Log("Mööh!");
+    }
+
+    public override string Talk()
+    {
+        return "Määh!";
+    }
+
+    public override GameObject GetEars()
+    {
+        GameObject ears = Resources.Load("CatEars") as GameObject;
+        return ears;
+    }
+}
